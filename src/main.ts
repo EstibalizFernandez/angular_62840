cab46a8251e447170c6740fe8ffa6167cc9f2654
Estibaliz Fernandez <estibalizfpalacios@gmail.com>
2024-12-17 23:23:04 +0100

    ejemplos de valores en typescript

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,17 @@ platformBrowserDynamic().bootstrapModule(AppModule, {
   ngZoneEventCoalescing: true,
 })
   .catch(err => console.error(err));
+
+  console.log("hola mundo");
+
+  // EJEMPLO VARIABLES EN TS
+
+  let number: number = 30;
+  let string: string = "Negua";
+  let boolean: boolean = false;
+  let nulo: null = null;
+  let any: any = undefined;
+  // distintos tipos de array
+  let arrayDeString: string[] = ["a", "b"];
+  let arrayMultiple: (number | string)[]= ["a", 2];
+  let tupla: [number, string]= [1, "b"];
